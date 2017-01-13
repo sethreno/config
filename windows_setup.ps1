b7ff14d6b5702efafb965de32d8b8fa5c5fd728e
Seth Reno <sethreno@gmail.com>
2017-01-12 20:11:40 -0600

    Add python for vim plugins

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,12 +1,15 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco install ConEmu
 choco install slickrun
-choco install vim
+choco install python2
+choco install python3
 choco install git
 choco install poshgit
+choco install vim
 choco install mssql2014express-defaultinstance
 
 # do this last, it might time out btw
 choco install visualstudio2015professional
 
+#manual install of vsvim - choco package fails
 
