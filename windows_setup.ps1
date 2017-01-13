f1754c4214072ab0dd83f190f8cbfabac4f794f9
Seth Reno <sethreno@gmail.com>
2017-01-12 19:25:39 -0600

    script install of windows dev apps

---
+++ windows_setup.ps1
@@ -0,0 +1,12 @@
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco install ConEmu
+choco install slickrun
+choco install vim
+choco install git
+choco install poshgit
+choco install mssql2014express-defaultinstance
+
+# do this last, it might time out btw
+choco install visualstudio2015professional
+
+
