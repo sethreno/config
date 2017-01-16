31b5c04da8a26a50e118e87d49a694eee0daa70d
Seth Reno <sethreno@gmail.com>
2017-01-16 10:23:42 -0600

    Add powershell profile script

---
+++ powershell_profile.ps1
@@ -0,0 +1,8 @@
+$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+Write-Host "persistent history enabled ($HistoryFilePath)"
+
+Set-PSReadlineOption -EditMode Vi
+Write-Host "vi mode enabled :D"
+
