eddc62e517ada03b082c6d54dad1dc69bb57e4bd
Seth Reno <sethreno@gmail.com>
2022-01-20 17:49:14 -0600

    disable starship & enable posh git

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,5 +1,18 @@
 
 Set-PSReadlineOption -EditMode Vi
 Import-Module PSColor
-Invoke-Expression (&starship init powershell)
 
+# function prompt {"> "}
+
+Import-Module posh-git
+
+# starship disabled due to performance bug
+#Invoke-Expression (&starship init powershell)
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
