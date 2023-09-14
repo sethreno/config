b16cc5bbbbdeafe01ee542ecb0ed5131f9321215
Seth Reno <sethreno@gmail.com>
2023-09-14 03:37:15 -0500

    update visual studio

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -8,13 +8,17 @@ choco install vscode --yes
 
 #choco install sql-server-express
 #choco install mssql2014express-defaultinstance --yes --verbose
-#choco install visualstudio2019professional --yes
+#choco install visualstudio2022professional --yes
 #manual install of the following becaue their choco package doesn't work or doesn't exist
 # vs addin - vsvim
 # vs addin - tabgroupjumber
 # vs addin - rebracer
 # vs addin - editorconfig
 # vs addin - resharper
+
+# install tab navigator extension then add keyboard shortcuts
+
+# https://github.com/jagt/vs-NavigateTabGroups/releases/tag/vs2022
 
 #enable colors for ls command
 Install-Module PSColor
